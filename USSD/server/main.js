02392af6b5db724325725c5fed52004d840da27e
jackyk <jackykimani13@gmail.com>
2019-01-24 11:16:03 +0000

    connect to db and help display count of the voters

--- USSD/server/main.js
+++ USSD/server/main.js
@@ -1,5 +1,28 @@
-import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
+
+import { ApolloServer } from 'apollo-server-express'
+
+import typeDefs from '../imports/api/schema'
+import resolvers from '../imports/api/resolvers'
+import '../imports/api/votes.js';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => ({
+    user: await getUser(req.headers.authorization)
+  })
+})
+
+server.applyMiddleware({
+  app: WebApp.connectHandlers,
+  path: '/graphql'
+})
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+  if (req.method === 'GET') {
+    res.end()
+  }
+})
